43f345fdd0f0194f833156a2033a15248b2926ba
Nigel Horne <njh@bandsman.co.uk>
2023-02-28 09:34:58 -0500

    Copied code from Geo-Coder-CA

---
+++ lib/Weather/Meteo.pm
@@ -0,0 +1,249 @@
+package Weather::Meteo
+
+use strict;
+use warnings;
+
+use Carp;
+use Encode;
+use JSON::MaybeXS;
+use HTTP::Request;
+use LWP::UserAgent;
+use LWP::Protocol::https;
+use URI;
+
+=head1 NAME
+
+Weather::Meteo - Interface to L<https://open-meteo.com> for historical weather data
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+      use Weather::Meteo
+
+      my $meteo = Weather::Meteo->new();
+      my $weather = $meteo->weather({ latitude => '0.1', longitude => '0.2', date => '25/12/2022' });
+
+=head1 DESCRIPTION
+
+Geo::Coder::CA provides an interface to geocoder.ca.
+
+=head1 METHODS
+
+=head2 new
+
+    $geo_coder = Geo::Coder::CA->new();
+    my $ua = LWP::UserAgent->new();
+    $ua->env_proxy(1);
+    $geo_coder = Geo::Coder::CA->new(ua => $ua);
+
+=cut
+
+sub new {
+	my($class, %args) = @_;
+
+	if(!defined($class)) {
+		# Geo::Coder::CA::new() used rather than Geo::Coder::CA->new()
+		$class = __PACKAGE__;
+	} elsif(ref($class)) {
+		# clone the given object
+		return bless { %{$class}, %args }, ref($class);
+	}
+
+	my $ua = $args{ua};
+	if(!defined($ua)) {
+		$ua = LWP::UserAgent->new(agent => __PACKAGE__ . "/$VERSION");
+		$ua->default_header(accept_encoding => 'gzip,deflate');
+	}
+	my $host = $args{host} || 'open-meteo.com';
+
+	return bless { ua => $ua, host => $host }, $class;
+}
+
+=head2 weather
+
+    $location = $geo_coder->geocode(location => $location);
+    # @location = $geo_coder->geocode(location => $location);
+
+    print 'Latitude: ', $location->{'latt'}, "\n";
+    print 'Longitude: ', $location->{'longt'}, "\n";
+
+=cut
+
+sub geocode {
+	my $self = shift;
+	my %param;
+
+	if(ref($_[0]) eq 'HASH') {
+		%param = %{$_[0]};
+	} elsif(ref($_[0])) {
+		Carp::croak('Usage: geocode(location => $location)');
+		return;
+	} elsif(@_ % 2 == 0) {
+		%param = @_;
+	} else {
+		$param{location} = shift;
+	}
+
+	my $location = $param{location};
+	if(!defined($location)) {
+		Carp::croak('Usage: geocode(location => $location)');
+		return;
+	}
+
+	if (Encode::is_utf8($location)) {
+		$location = Encode::encode_utf8($location);
+	}
+
+	my $uri = URI->new("https://$self->{host}/some_location");
+	$location =~ s/\s/+/g;
+	my %query_parameters = ('locate' => $location, 'json' => 1, 'strictmode' => 1);
+	$uri->query_form(%query_parameters);
+	my $url = $uri->as_string();
+
+	my $res = $self->{ua}->get($url);
+
+	if($res->is_error()) {
+		Carp::croak("$url API returned error: ", $res->status_line());
+		return;
+	}
+	# $res->content_type('text/plain');	# May be needed to decode correctly
+
+	my $json = JSON::MaybeXS->new()->utf8();
+	if(my $rc = $json->decode($res->decoded_content())) {
+		if($rc->{'error'}) {
+			# Sorry - you lose the error code, but HTML::GoogleMaps::V3 relies on this
+			# TODO - send patch to the H:G:V3 author
+			return;
+		}
+		if(defined($rc->{'latt'}) && defined($rc->{'longt'})) {
+			return $rc;	# No support for list context, yet
+		}
+
+		# if($location =~ /^(\w+),\+*(\w+),\+*(USA|US|United States)$/i) {
+			# $query_parameters{'locate'} = "$1 County, $2, $3";
+			# $uri->query_form(%query_parameters);
+			# $url = $uri->as_string();
+			#
+			# $res = $self->{ua}->get($url);
+			#
+			# if($res->is_error()) {
+				# Carp::croak("geocoder.ca API returned error: " . $res->status_line());
+				# return;
+			# }
+			# return $json->decode($res->content());
+		# }
+	}
+
+	# my @results = @{ $data || [] };
+	# wantarray ? @results : $results[0];
+}
+
+=head2 ua
+
+Accessor method to get and set UserAgent object used internally. You
+can call I<env_proxy> for example, to get the proxy information from
+environment variables:
+
+    $geo_coder->ua()->env_proxy(1);
+
+You can also set your own User-Agent object:
+
+    my $ua = LWP::UserAgent::Throttled->new();
+    $ua->throttle('geocoder.ca' => 1);
+    $geo_coder->ua($ua);
+
+=cut
+
+sub ua {
+	my $self = shift;
+	if (@_) {
+		$self->{ua} = shift;
+	}
+	$self->{ua};
+}
+
+=head2 reverse_geocode
+
+    $location = $geo_coder->reverse_geocode(latlng => '37.778907,-122.39732');
+
+Similar to geocode except it expects a latitude/longitude parameter.
+
+=cut
+
+sub reverse_geocode {
+	my $self = shift;
+
+	my %param;
+	if (@_ % 2 == 0) {
+		%param = @_;
+	} else {
+		$param{latlng} = shift;
+	}
+
+	my $latlng = $param{latlng}
+		or Carp::croak('Usage: reverse_geocode(latlng => $latlng)');
+
+	return $self->geocode(location => $latlng, reverse => 1);
+}
+
+=head2 run
+
+You can also run this module from the command line:
+
+    perl CA.pm 1600 Pennsylvania Avenue NW, Washington DC
+
+=cut
+
+__PACKAGE__->run(@ARGV) unless caller();
+
+sub run {
+	require Data::Dumper;
+
+	my $class = shift;
+
+	my $location = join(' ', @_);
+
+	my @rc = $class->new()->geocode($location);
+
+	if(scalar(@rc)) {
+		print Data::Dumper->new([\@rc])->Dump();
+	} else {
+		die "$0: geo-coding failed";
+	}
+}
+
+=head1 AUTHOR
+
+Nigel Horne, C<< <njh@bandsman.co.uk> >>
+
+Based on L<Geo::Coder::Googleplaces>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+Lots of thanks to the folks at geocoder.ca.
+
+=head1 BUGS
+
+Should be called Geo::Coder::NA for North America.
+
+=head1 SEE ALSO
+
+L<Geo::Coder::GooglePlaces>, L<HTML::GoogleMaps::V3>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2017-2023 Nigel Horne.
+
+This program is released under the following licence: GPL2
+
+=cut
+
+1;

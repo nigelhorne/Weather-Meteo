2d9723889e6ee376213d1bb423124b741104a1ba
Nigel Horne <njh@bandsman.co.uk>
2023-02-28 11:58:10 -0500

    Release 0.02

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Weather::Meteo - Interface to [https://open-meteo.com](https://open-meteo.com) f
 
 # VERSION
 
-Version 0.01
+Version 0.02
 
 # SYNOPSIS
 
--- lib/Weather/Meteo.pm
+++ lib/Weather/Meteo.pm
@@ -17,11 +17,11 @@ Weather::Meteo - Interface to L<https://open-meteo.com> for historical weather d
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
